64b1d3e69a36690d8e843cc122996a8a636d3ac3
xp-dx <salnikov-ds@mail.ru>
2024-12-23 22:08:04 +0300

    Albums

--- backend/src/albums/crud.py
+++ backend/src/albums/crud.py
@@ -1,10 +1,65 @@
 from sqlalchemy.orm import Session
 
+import json
+
 from src.auth import schemas as _user_schemas
 
 from . import schemas as _schemas
 
 from .. import models as _global_models
+
+
+def read_all_albums(db: Session):
+    db_albums = db.query(_global_models.Album).all()
+    albums_json = []
+    for album in db_albums:
+        albums_json.append(
+            {"id": album.id, "title": album.title, "description": album.description}
+        )
+    return json.loads(json.dumps(albums_json, default=str))
+
+
+# def read_album(db: Session, album_id: int):
+#     db_album = (
+#         db.query(_global_models.Album)
+#         .filter(_global_models.Album.id == album_id)
+#         .first()
+#     )
+#     return db_album
+
+
+def read_album_songs(db: Session, album_id: int):
+    db_album = (
+        db.query(_global_models.Album)
+        .filter(_global_models.Album.id == album_id)
+        .first()
+    )
+
+    db_album_songs = (
+        db.query(_global_models.Song)
+        .filter(_global_models.Song.album_id == album_id)
+        .all()
+    )
+    album_songs_json = {
+        "album": {
+            "id": db_album.id,
+            "title": db_album.title,
+            "description": db_album.description,
+        },
+        "songs": [],
+    }
+
+    for album_song in db_album_songs:
+        album_songs_json["songs"].append(
+            {
+                "id": album_song.id,
+                "title": album_song.title,
+                "artist": album_song.artist,
+                "genre": album_song.genre,
+                "price": album_song.price,
+            }
+        )
+    return json.loads(json.dumps(album_songs_json, default=str))
 
 
 def create_album(db: Session, user: _user_schemas.UserId, album: _schemas.Album):
--- backend/src/albums/router.py
+++ backend/src/albums/router.py
@@ -6,11 +6,26 @@ from typing import Annotated
 
 from . import schemas as _schemas, crud as _crud
 
-from .. import dependencies as _global_dependencies
+from .. import dependencies as _global_dependencies, models as _global_models
 
 from src.auth import dependencies as _auth_dependencies, schemas as _auth_schemas
 
 router = APIRouter(tags=["albums"])
+
+
+@router.get("/albums")
+def read_albums(db: Session = Depends(_global_dependencies.get_db)):
+    return _crud.read_all_albums(db=db)
+
+
+# @router.get("/album/{album_id}")
+# def read_album(album_id: int, db: Session = Depends(_global_dependencies.get_db)):
+#     return _crud.read_album(db=db, album_id=album_id)
+
+
+@router.get("/album/{album_id}")
+def read_album(album_id: int, db: Session = Depends(_global_dependencies.get_db)):
+    return _crud.read_album_songs(db=db, album_id=album_id)
 
 
 @router.post("/create-album")
