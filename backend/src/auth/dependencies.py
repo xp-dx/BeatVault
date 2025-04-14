0b5339a9fc147c9afdeb7b0765a1ec5d2f3ebbf6
xp-dx <salnikov-ds@mail.ru>
2025-04-14 22:22:37 +0300

    fix(auth/dependencies.py): add missing await to get_user_by_username in get_current_user

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -21,7 +21,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-def get_current_user(
+async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: Session = Depends(_global_dependencies.get_async_session),
 ):
@@ -39,7 +39,7 @@ def get_current_user(
 
     except InvalidTokenError:
         raise credentials_exception
-    user = _service.get_user_by_username(db, username=token_data.username)
+    user = await _service.get_user_by_username(db, username=token_data.username)
     # user_json = [
     #     {
     #         "id": user.id,
@@ -60,6 +60,7 @@ def get_current_user(
 def get_current_active_user(
     current_user: Annotated[_schemas.User, Depends(get_current_user)],
 ):
+    print(current_user)
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
