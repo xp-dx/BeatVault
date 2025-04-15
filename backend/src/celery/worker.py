f6137e38fc35aebceb0dd0f761c87853c81dc082
xp-dx <salnikov-ds@mail.ru>
2025-04-15 19:08:39 +0300

    chore: remove obsolete folders and clean up codebase

--- backend/src/celery/worker.py
+++
@@ -1,4 +0,0 @@
-from src.celery import celery_app
-
-if __name__ == "__main__":
-    celery_app.worker_main()
--- backend/src/sms/tasks.py
+++
@@ -1,9 +0,0 @@
-# from celery import Celery
-# from sms.service import send_sms_code
-
-# celery_app = Celery("worker", broker="redis://localhost:6379/0")
-
-
-# @celery_app.task
-# def send_sms_async(phone: str):
-#     send_sms_code(phone)
